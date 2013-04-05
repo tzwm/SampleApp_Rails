bf147f02f3de0cf133d851f4a94520a0de79a2ce
tzwm <wangjunzhe4444@gmail.com>
2013-04-05 08:58:37 +0800

    fix bug at users_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_filter :current_user, only: [:edit, :update]
-  before_filter :admin_user, only :destroy
+  before_filter :admin_user, only: :destroy
 
   def show
   	@user = User.find(params[:id])
