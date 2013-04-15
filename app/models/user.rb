610bebf706c0b1dabc451c542d629e999b2ab4de
tzwm <wangjunzhe4444@gmail.com>
2013-04-15 16:03:27 +0800

    add userid to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,20 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
+#  userid          :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :userid
+  
   has_secure_password
   
   has_many :microposts, dependent: :destroy
@@ -33,6 +38,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  validates :userid, presence: true, length: { maximum: 50 },
+            uniqueness: { case_sensitive: false}
+
+
   def feed 
     # Micropost.where("user_id = ?", id)
     Micropost.from_users_followed_by(self)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410151547) do
+ActiveRecord::Schema.define(:version => 20130415065820) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(:version => 20130410151547) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
+    t.string   "userid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  add_index "users", ["userid"], :name => "index_users_on_userid"
 
 end
